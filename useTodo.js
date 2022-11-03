3944a3348a07a6832776e96dd9a6376064a52007
ricardo di benedetto <richard.u.db@gmail.com>
2022-11-03 12:53:22 -0300

    first commit

---
+++ useTodo.js
@@ -0,0 +1,41 @@
+import { useReducer } from "react";
+import { todoReducer } from "../08-reducer/todoReducer"
+const initialState = [];
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || []
+}
+
+export const useTodo = () => {
+
+    const [todos, dispatchTodo] = useReducer(todoReducer, [], init);
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: "[TODO] add todo",
+            payload: todo
+        }
+        dispatchTodo(action);
+    }
+    const handleRemoveTodo = (id) => {
+        dispatchTodo({
+            type: "[TODO] Remove Todo",
+            payload: id
+        });
+    }
+    const handleToggleTodo = (id) => {
+        dispatchTodo({
+            type: "[TODO] Done Todo",
+            payload: id
+        });
+    }
+
+    return {
+        todos,
+        todoCount: todos.length,
+        pendingTodoCount: todos.filter( todo => !todo.done).length,
+        handleNewTodo,
+        handleRemoveTodo,
+        handleToggleTodo ,
+    }
+}
